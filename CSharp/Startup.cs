cdcddfceebb1a5dea7191c5ee6edd8d6d2f3dcba
halyhuang1991 <18807898837@139.com>
2019-04-02 12:38:05 +0800

    1

--- CSharp/Startup.cs
+++ CSharp/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Consul;
 using CSharp.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
@@ -65,7 +66,31 @@ namespace CSharp
             //app.UseCors(builder => builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod().AllowCredentials());
             app.UseAuthentication();
             app.UseMvc();
-            
+
+            String ip = "127.0.0.1";//Configuration["ip"];//部署到不同服务器的时候不能写成127.0.0.1或者0.0.0.0,因为这是让服务消费者调用的地址
+            Int32 port = 5000;//Int32.Parse(Configuration["port"]);
+            //向consul注册服务
+            ConsulClient client = new ConsulClient(ConfigurationOverview);
+            Task<WriteResult> result = client.Agent.ServiceRegister(new AgentServiceRegistration()
+            {
+                ID = "apiservice1" + Guid.NewGuid(),//服务编号，不能重复，用Guid最简单
+                Name = "apiservice1",//服务的名字
+                Address = ip,//我的ip地址(可以被其他应用访问的地址，本地测试可以用127.0.0.1，机房环境中一定要写自己的内网ip地址)
+                Port = port,//我的端口
+                Check = new AgentServiceCheck()
+                {
+                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//服务停止多久后反注册
+                    Interval = TimeSpan.FromSeconds(10),//健康检查时间间隔，或者称为心跳间隔
+                    HTTP = $"http://{ip}:{port}/api/Account?usrname=halyhuang",//健康检查地址,
+                    Timeout = TimeSpan.FromSeconds(5)
+                }
+            });
+           
+        }
+        private static void ConfigurationOverview(ConsulClientConfiguration obj)
+        {
+            obj.Address = new Uri("http://127.0.0.1:8500");
+            obj.Datacenter = "dc1";
         }
     }
    
